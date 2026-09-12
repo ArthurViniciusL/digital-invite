a14b2eea588107fc14a2764be4d192a479a52fd0
arthur.lucena <arthur.vsl.contato@gmail.com>
2026-09-12 12:52:10 -0300

    Implemente the third section with basic modal

--- src/pages/InvitePage/index.tsx
+++ src/pages/InvitePage/index.tsx
@@ -1,17 +1,14 @@
 import { PageWrapper } from '@/components/layout/PageWrapper'
 import { EventDetails } from './partials/EventDetails'
 import { InviteHero } from './partials/InviteHero'
+import { RsvpForm } from './partials/RsvpForm'
 
-/**
- * Public invite page, served at `/`.
- *
- * TODO: compose `RsvpForm` once it exists.
- */
 export function InvitePage() {
   return (
     <PageWrapper>
       <InviteHero />
       <EventDetails />
+      <RsvpForm />
     </PageWrapper>
   )
 }
